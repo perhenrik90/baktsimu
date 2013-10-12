f9c494ec6606001b09e4c2ed493a187354b94bd1
PH <perhenrik.kvalnes@gmail.com>
2013-10-12 16:31:39 +0200

    fikset fysikken

--- bakterie.js
+++ bakterie.js
@@ -4,28 +4,29 @@
  */
  var __fysikk__;
  __fysikk__ = true;
+ __MAX_WIDTH__ = 21;
 
 function createBakterie(x_pos, y_pos)
 {
-	    var texture 	= PIXI.Texture.fromImage("bakterie.png");
+	var texture 	= PIXI.Texture.fromImage("bakterie.png");
         var bakt    	= new PIXI.Sprite( texture );
         bakt.anchor.x 	= 0.5;
         bakt.anchor.y 	= 0.5;
         bakt.position.x = x_pos;
         bakt.position.y = y_pos;
-	bakt.width	= 20;
-	bakt.height	= 20;
+	bakt.width	= 10 + parseInt(Math.random()*__MAX_WIDTH__);
+	bakt.height	= bakt.width;
 	bakt.spinn	= (Math.random()-0.5)*0.1;
-		
+	
 		/** setup next position updater ***/
-		bakt.x	= (Math.random()-0.5)*0.5;
-		bakt.y	= (Math.random()-0.5)*0.5;
+		bakt.x	= (Math.random()-0.6)*0.5;
+		bakt.y	= (Math.random()-0.6)*0.5;
 
 		bakt.update = function(bakt)
 		{
-			g_faktor = 0.00008;
-			g_max	 = 20;
-			g_min    = 6;
+			g_faktor = 0.000002;
+			g_max	 = 45;
+			g_min    = 2;
 
 
 			/** enekel fysikk ? **/	
@@ -33,55 +34,73 @@ function createBakterie(x_pos, y_pos)
 			{
 				for(i in stage.children)
 				{
-					rnd_faktor = Math.random()*2;
+					rnd_faktor = 1;
 					nabo	= stage.children[i];
 
+					if(nabo == bakt){break;}
+
 					/** x -akse **/
-					nx      = (nabo.position.x - bakt.position.x);	
-					nx	-= (Math.abs(nabo.position.y - bakt.position.y));
- 
-					if(Math.abs(nx) < g_max && Math.abs(nx) > g_min)
+					nnx     = (bakt.position.x-nabo.position.x);//+nabo.width;	
+					
+					if(Math.abs(nnx) < g_max && Math.abs(nnx) > g_min)
+					{
+						nx	= Math.abs(nnx-width);		// invert
+						if(nnx < 0){nx = -nx;}
+						nx 	= nx * (nabo.width/__MAX_WIDTH__);
 						bakt.x  += (nx*g_faktor)*rnd_faktor;
+					}
+
+
 
 					/** y - akse **/
-					ny		= (nabo.position.y - bakt.position.y);
-					ny	-= (nabo.position.x - bakt.position.x);
-					if(Math.abs(ny) < g_max && Math.abs(ny) > g_min)
+					nny	= (bakt.position.y - nabo.position.y);
+
+					if(Math.abs(nny) < g_max && Math.abs(nny) > g_min)
+					{
+						ny	= Math.abs(nny-height);		// invert
+						if(nny < 0){ny = -ny;}
+						ny 	= ny * (nabo.width/__MAX_WIDTH__);
+
 						bakt.y  += (ny*g_faktor)*rnd_faktor;
-				}	
+						
+					}
+				}
+				
+				bakt.x = bakt.x * 0.9999;
+				bakt.y = bakt.y * 0.9999;	
 			}
 
 
 			/*** Collision detection **/
 			if(bakt.position.x > renderer.view.width)
 			{
-				bakt.x = -(Math.random()*0.5);	
+				bakt.x = (Math.random()*1);	
 			}
 			else if(bakt.position.x < 0)
 			{		
-				bakt.x = Math.random()*0.5;
+				bakt.x = -Math.random()*1;
 			}	
 
 			if(bakt.position.y > renderer.view.height)
 			{
-		 		bakt.y = -(Math.random()*0.25);
+		 		bakt.y = (Math.random()*1);
 			}
 			else if(bakt.position.y < 0)
 			{
-				bakt.y = Math.random()*0.25;
+				bakt.y = -Math.random()*1;
 			}	
 
 
 			/** update position **/
 			bakt.rotation   += bakt.spinn
-			bakt.position.x += bakt.x;
-			bakt.position.y += bakt.y;
+			bakt.position.x -= bakt.x;
+			bakt.position.y -= bakt.y;
 		}
 
 
 		function kill(e)
 		{
-			console.log(e);
+			document.getElementById("antallLabel").innerHTML = "Antall bakterier: "+list.length;
 			stage.removeChild(e.target);
 		}
 		bakt.mouseover  = kill;
